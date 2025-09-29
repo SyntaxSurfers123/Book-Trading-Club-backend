09bf5d9f8c9a322fc017205d3c93d3ef3740d02c
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-09-29 11:40:03 +0600

    BOOK CRUD done

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import BookRoutes from './Routes/BookRoutes.js';
 import { connectDB } from './Config/db.js';
+dotenv.config();
 // Middlewares and configurations
 const app = express();
-dotenv.config();
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 3001;
+
+// Quick health check
+app.get('/health', (_req, res) => res.send('ok'));
 
 /////////////////////// Routes //////////////////////////////////
 app.use('/api/books', BookRoutes);
@@ -21,7 +24,7 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Welcome to the Book Trading Club server!');
 });
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+export default app;
+// app.listen(PORT, () => {
+//   console.log(`Listening on port ${PORT}...`);
+// });
